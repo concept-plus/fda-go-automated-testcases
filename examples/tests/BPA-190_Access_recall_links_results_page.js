da07c923f3378183deb2fb8c9962039b37b3b920
Alex Rangeo <arangeo@conceptplusllc.com>
2015-07-07 03:05:14 -0400

    update 190

--- examples/tests/BPA-190_Access_recall_links_results_page.js
+++ examples/tests/BPA-190_Access_recall_links_results_page.js
@@ -14,21 +14,14 @@ module.exports = {
 			.click(client.globals.srchbtnID)
         client.globals.TakeSS(client, 'BPA-190- recall_links_mainpage');
    },
-    'Click on Drug recalls button and verify drug results come up': function(client) {
-        client
-            .click(client.globals.sidebarDrugRecallBtnID)
-            .pause(5000)
-            .verify.elementPresent('#event-results-table tr');
-
-        client.globals.TakeSS(client, 'BPA-190-drug_recall_results');
-    },
+   
 	'Click on Device recalls button and verify devices results come up': function(client) {
         client
 		    .click(client.globals.sidebarDeviceRecallBtnID)
             .pause(5000)
             .verify.elementPresent('#label-results-table tr');
 
-        client.globals.TakeSS(client, 'BPA-190-device_recall_results');
+        
     },
 	'Click on food recalls button and verify food come up': function(client) {
         client
@@ -36,7 +29,6 @@ module.exports = {
             .pause(5000)
             .verify.elementPresent('#recall-results-table tr');
 
-        client.globals.TakeSS(client, 'BPA-190-food_recall_results');
     },
 
     'Close Client': function(client) {
